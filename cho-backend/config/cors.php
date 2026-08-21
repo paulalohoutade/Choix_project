26de20af9d7e420db89a8fa1692caaeed38ecc42
paulalohoutade <paulalohoutade7@gmail.com>
2026-08-21 18:31:24 +0100

    modificato

--- cho-backend/config/cors.php
+++ cho-backend/config/cors.php
@@ -9,7 +9,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://chorale-hefzibah.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
