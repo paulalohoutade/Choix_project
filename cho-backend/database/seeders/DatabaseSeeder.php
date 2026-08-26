98886e520314ce1d7b8d41b823f0c902f5cd2b8c
paulalohoutade <paulalohoutade7@gmail.com>
2026-08-26 15:04:35 +0100

    fix: clear albums, tracks, gallery on seed to reset production data

--- cho-backend/database/seeders/DatabaseSeeder.php
+++ cho-backend/database/seeders/DatabaseSeeder.php
@@ -3,12 +3,20 @@
 
 namespace Database\Seeders;
 
+use App\Models\Album;
+use App\Models\GalleryItem;
+use App\Models\Track;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        // Vider le contenu avant de reséeder
+        Track::truncate();
+        Album::truncate();
+        GalleryItem::truncate();
+
         $this->call([
             AdminUserSeeder::class,
             SettingSeeder::class,
